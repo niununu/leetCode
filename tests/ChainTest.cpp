4a93994bf1eb364216a9fb436033c6286b8b82b1
niununu <vivianliulu@126.com>
2021-12-21 00:06:40 +0800

    update chain
    update chain

--- tests/ChainTest.cpp
+++ tests/ChainTest.cpp
@@ -1,7 +1,8 @@
 #include <catch2/catch_test_macros.hpp>
 #include "../include/Chain.h"
 #include <cmath>
-
+#include <iostream>
+#include <vector>
 TEST_CASE("Solution", "[ChainTest]")
 {
     Solution so;
@@ -13,22 +14,59 @@ TEST_CASE("Solution", "[ChainTest]")
         REQUIRE(so.pow(10,10) == pow(10,10));
     }
 
-    SECTION("convertToNum")
+    SECTION("reversList")
     {
-        struct ListNode* list1 = new struct ListNode;
-        auto num1 = so.listToNum(list1);
-        REQUIRE(num1 == 0);
+        auto getTestList = [](std::vector<int> vals){
+            struct ListNode* head = nullptr;
+            struct ListNode* prv = nullptr;
+            for(const auto val : vals)
+            {
+                struct ListNode* list = new struct ListNode(val);
+                if (!head)
+                {
+                    head = list;
+                }
+                if (prv)
+                {
+                    prv->next = list;
+                }
+                prv = list;
 
-        struct ListNode* list2 = new struct ListNode;
-        list2->val = (2);
-        struct ListNode* tmp1 = new struct ListNode;
-        tmp1->val = (4);
-        struct ListNode* tmp2 = new struct ListNode;
-        tmp2->val = (3);
-        tmp1->next = tmp2;
-        list2->next = tmp1;
+            }
+            auto tmp = head;
+            while(tmp)
+            {
+                std::cout << tmp->val << " ";
+                tmp = tmp->next;
+            }
+            std::cout << std::endl;
+            return head;
+        };
+        // struct ListNode* list1 = new struct ListNode;
+        // auto num1 = so.listToNum(list1);
+        // REQUIRE(num1 == 0);
 
-        auto num2 = so.listToNum(list2);
-        REQUIRE(num2 == 342);
+        // struct ListNode* list2 = new struct ListNode(2);
+        // struct ListNode* tmp1 = new struct ListNode(4);
+        // struct ListNode* tmp2 = new struct ListNode(3);
+        // tmp1->next = tmp2;
+        // list2->next = tmp1;
+
+        // struct ListNode* list3 = new struct ListNode(5);
+        // struct ListNode* tmp3 = new struct ListNode(6);
+        // struct ListNode* tmp4 = new struct ListNode(4);
+        // tmp3->next = tmp4;
+        // list3->next = tmp3;
+        auto list1 = getTestList({0,8,6,5,6,8,3,5,7});
+        auto list2 = getTestList({9,9,9,9});
+//                auto list1 = getTestList({5});
+//                auto list2 = getTestList({5});
+        auto list = so.addTwoNumbers(list2, list1);
+        while(list)
+        {
+            std::cout << list->val << " ";
+            list = list->next;
+        }
+        // REQUIRE(num2 == 342);
     }
 }
