adeffcf3f460d84a902cbb77b346e6a1abd46af2
luliu5 <luliu5@cisco.com>
2022-01-06 20:59:51 +0800

    0106
    0106

--- include/Solution_Chain.h
+++ include/Solution_Chain.h
@@ -24,11 +24,11 @@ public:
         ListNode(int x, ListNode *next) : val(x), next(next) {}
     };
 
-    ListNode* createChain(vector<int> vals)
+    ListNode *createChain(vector<int> vals)
     {
-        ListNode* head = nullptr;
-        ListNode* cur = nullptr;
-        for (const auto& val : vals)
+        ListNode *head = nullptr;
+        ListNode *cur = nullptr;
+        for (const auto &val : vals)
         {
             auto node = new ListNode(val);
             if (!head)
@@ -46,10 +46,10 @@ public:
         return head;
     }
 
-    vector<int> printChain(ListNode* head)
+    vector<int> printChain(ListNode *head)
     {
         vector<int> vals;
-        while(head)
+        while (head)
         {
             cout << head->val << " ";
             vals.push_back(head->val);
@@ -58,15 +58,15 @@ public:
         cout << endl;
         return vals;
     }
-/*
-删除链表中的节点
-请编写一个函数，用于 删除单链表中某个特定节点 。在设计函数时需要注意，你无法访问链表的头节点 head ，只能直接访问要被删除的节点 。
-题目数据保证需要删除的节点 不是末尾节点 。
-*/
+    /*
+    删除链表中的节点
+    请编写一个函数，用于 删除单链表中某个特定节点 。在设计函数时需要注意，你无法访问链表的头节点 head ，只能直接访问要被删除的节点 。
+    题目数据保证需要删除的节点 不是末尾节点 。
+    */
 public:
     void deleteNode(ListNode *node)
     {
-        if(!node->next)
+        if (!node->next)
         {
             return;
         }
@@ -74,12 +74,13 @@ public:
         node->val = node->next->val;
         node->next = node->next->next;
     }
-/*
-删除链表的倒数第N个节点
-给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
-*/
+    /*
+    删除链表的倒数第N个节点
+    给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+    */
 public:
-    ListNode* removeNthFromEnd(ListNode* head, int n) {
+    ListNode *removeNthFromEnd(ListNode *head, int n)
+    {
         if (!head->next && n > 0)
         {
             return nullptr;
@@ -98,8 +99,8 @@ public:
             }
         }
 
-        ListNode* preNode = nullptr;
-        while(endNode)
+        ListNode *preNode = nullptr;
+        while (endNode)
         {
             if (!preNode)
             {
@@ -117,7 +118,7 @@ public:
         {
             head = head->next;
         }
-        else if(preNode && removeNode && !removeNode->next)
+        else if (preNode && removeNode && !removeNode->next)
         {
             preNode->next = nullptr;
         }
@@ -126,21 +127,21 @@ public:
             preNode->next = removeNode->next;
         }
 
-
         return head;
     } // removeNthFromEnd
-/*
-反转链表
-给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。*/
+    /*
+    反转链表
+    给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。*/
 public:
-    ListNode* reverseList(ListNode* head) {
+    ListNode *reverseList(ListNode *head)
+    {
         if (!head)
         {
             return head;
         }
         auto begin = head;
         auto next = head->next;
-        while(head && next)
+        while (head && next)
         {
             auto nextNext = next->next;
             next->next = head;
@@ -150,4 +151,134 @@ public:
         begin->next = nullptr;
         return head;
     } // reverseList
+
+    /*
+    合并两个有序链表
+    将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的
+    */
+public:
+    ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
+    {
+        if (!list1 || !list2)
+        {
+            return list1 ? list1 : list2;
+        }
+
+        ListNode *list = nullptr;
+        ListNode *insertList = nullptr;
+        if (list1->val <= list2->val)
+        {
+            list = list1;
+            insertList = list2;
+        }
+        else
+        {
+            list = list2;
+            insertList = list1;
+        }
+        ListNode *head = list;
+
+        ListNode *preNode = nullptr;
+        while (insertList && list)
+        {
+            if (insertList->val < list->val)
+            {
+                preNode->next = insertList;
+                auto tmp = insertList->next;
+                insertList->next = list;
+                preNode = insertList;
+                insertList = tmp;
+            }
+            else
+            {
+                if (!preNode)
+                {
+                    preNode = list;
+                }
+                else
+                {
+                    preNode = preNode->next;
+                }
+                list = list->next;
+            }
+        }
+
+        if (insertList && preNode)
+        {
+            preNode->next = insertList;
+        }
+
+        return head;
+    } // mergeTwoLists
+
+    /*
+    回文链表
+    给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。如果是，返回 true ；否则，返回 false 。
+    */
+public:
+    bool isPalindrome(ListNode *head)
+    {
+        bool result = true;
+        auto fast = head;
+        auto mid = head;
+        while (fast && fast->next)
+        {
+            fast = fast->next->next;
+            mid = mid->next;
+        }
+        auto begin = mid;
+        if (begin)
+        {
+            auto next = begin->next;
+            while (begin && next)
+            {
+                auto nextNext = next->next;
+                next->next = begin;
+                begin = next;
+                next = nextNext;
+            }
+            mid->next = nullptr;
+        }
+
+        while (begin && head)
+        {
+            if (begin->val != head->val)
+            {
+                result = false;
+                break;
+            }
+            else
+            {
+                begin = begin->next;
+                head = head->next;
+            }
+        }
+
+        return result;
+
+    } // isPalindrome
+
+    /*
+    环形链表
+    给你一个链表的头节点 head ，判断链表中是否有环。
+    如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。 为了表示给定链表中的环，评测系统内部使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。如果 pos 是 -1，则在该链表中没有环。注意：pos 不作为参数进行传递，仅仅是为了标识链表的实际情况。
+    如果链表中存在环，则返回 true 。 否则，返回 false 。
+    */
+public:
+    bool hasCycle(ListNode *head)
+    {
+        while (head)
+        {
+            if (head->val != INT_MAX)
+            {
+                head->val = INT_MAX;
+                head = head->next;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        return false;
+    } // hasCycle
 };
--- tests/Solution_ChainTests.cpp
+++ tests/Solution_ChainTests.cpp
@@ -12,21 +12,23 @@ public:
         so = Solution();
     }
     Solution so;
-    int expectResult{ 0 };
+    int expectResult{0};
     std::vector<int> input;
+    std::vector<int> input2;
     std::vector<int> output;
     int index = 0;
-
+    bool outputBool = false;
 
 }; // class SolutionChainTestsFixture
 
-TEST_CASE_METHOD(SolutionChainTestsFixture, "removeNthFromEnd", "[removeNthFromEnd]""[Solution_ChainTests]")
+TEST_CASE_METHOD(SolutionChainTestsFixture, "removeNthFromEnd", "[removeNthFromEnd]"
+                                                                "[Solution_ChainTests]")
 {
     SECTION("1")
     {
-        input = {1,2,3,4,5};
+        input = {1, 2, 3, 4, 5};
         index = 2;
-        output = {1,2,3,5};
+        output = {1, 2, 3, 5};
     }
 
     SECTION("2")
@@ -37,7 +39,7 @@ TEST_CASE_METHOD(SolutionChainTestsFixture, "removeNthFromEnd", "[removeNthFromE
     }
     SECTION("3")
     {
-        input = {1,2};
+        input = {1, 2};
         index = 1;
         output = {1};
     }
@@ -49,27 +51,28 @@ TEST_CASE_METHOD(SolutionChainTestsFixture, "removeNthFromEnd", "[removeNthFromE
     }
     SECTION("5")
     {
-        input = {1,2,3,4,5};
+        input = {1, 2, 3, 4, 5};
         index = 5;
-        output = {2,3,4,5};
+        output = {2, 3, 4, 5};
     }
 
-    ListNode* head = so.createChain(input);
+    ListNode *head = so.createChain(input);
     auto result = so.removeNthFromEnd(head, index);
     REQUIRE(SolutionTestUtil::isSameVector(so.printChain(result), output));
 }
 
-TEST_CASE_METHOD(SolutionChainTestsFixture, "reverseList", "[reverseList]""[Solution_ChainTests]")
+TEST_CASE_METHOD(SolutionChainTestsFixture, "reverseList", "[reverseList]"
+                                                           "[Solution_ChainTests]")
 {
     SECTION("1")
     {
-        input = {1,2,3,4,5};
-        output = {5,4,3,2,1};
+        input = {1, 2, 3, 4, 5};
+        output = {5, 4, 3, 2, 1};
     }
     SECTION("2")
     {
-        input = {1,2};
-        output = {2,1};
+        input = {1, 2};
+        output = {2, 1};
     }
     SECTION("3")
     {
@@ -84,4 +87,88 @@ TEST_CASE_METHOD(SolutionChainTestsFixture, "reverseList", "[reverseList]""[Solu
     auto head = so.createChain(input);
     auto result = so.reverseList(head);
     REQUIRE(SolutionTestUtil::isSameVector(output, so.printChain(result)));
+}
+
+TEST_CASE_METHOD(SolutionChainTestsFixture, "mergeTwoLists", "[mergeTwoLists]"
+                                                             "[Solution_ChainTests]")
+{
+    SECTION("")
+    {
+        input = {1, 2, 4};
+        input2 = {1, 3, 4};
+        output = {1, 1, 2, 3, 4, 4};
+    }
+    SECTION("")
+    {
+        input = {};
+        input2 = {};
+        output = {};
+    }
+    SECTION("")
+    {
+        input = {};
+        input2 = {0};
+        output = {0};
+    }
+    SECTION("")
+    {
+        input = {};
+        input2 = {0, 1, 2, 3};
+        output = {0, 1, 2, 3};
+    }
+    SECTION("")
+    {
+        input = {0, 1, 2, 3};
+        input2 = {};
+        output = {0, 1, 2, 3};
+    }
+    SECTION("")
+    {
+        input = {0};
+        input2 = {1, 2, 3};
+        output = {0, 1, 2, 3};
+    }
+    SECTION("")
+    {
+        input2 = {0};
+        input = {1, 2, 3};
+        output = {0, 1, 2, 3};
+    }
+    SECTION("")
+    {
+        input = {1, 2, 3};
+        input2 = {1, 2, 3};
+        output = {1, 1, 2, 2, 3, 3};
+    }
+    auto list1 = so.createChain(input);
+    auto list2 = so.createChain(input2);
+    auto result = so.mergeTwoLists(list1, list2);
+    REQUIRE(SolutionTestUtil::isSameVector(output, so.printChain(result)));
+}
+
+TEST_CASE_METHOD(SolutionChainTestsFixture, "isPalindrome", "[isPalindrome]"
+                                                            "[Solution_ChainTests]")
+{
+    SECTION("")
+    {
+        input = {1, 2, 2, 1};
+        outputBool = true;
+    }
+    SECTION("")
+    {
+        input = {1, 2, 3, 2, 1};
+        outputBool = true;
+    }
+    SECTION("")
+    {
+        input = {1, 2};
+        outputBool = false;
+    }
+    SECTION("")
+    {
+        input = {1};
+        outputBool = true;
+    }
+    auto head = so.createChain(input);
+    REQUIRE(so.isPalindrome(head) == outputBool);
 }
