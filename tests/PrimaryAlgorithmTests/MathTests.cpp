83051b70824d34b8c374b32e964a2b0f8bf9cc2d
luliu5 <luliu5@cisco.com>
2022-01-12 20:57:23 +0800

    01-12
    01-12

---
+++ tests/PrimaryAlgorithmTests/MathTests.cpp
@@ -0,0 +1,24 @@
+#include <catch2/catch_test_macros.hpp>
+#include "../include/PrimaryAlgorithm/Math.h"
+#include "../TestUtils.h"
+
+class MathTestsFixture
+{
+public:
+    MathTestsFixture()
+    {
+        so = Solution();
+    }
+    Solution so;
+    // int input1{0};
+    // int input2{0};
+    // int output{0};
+    // uint32_t input{0};
+
+    // std::vector<int> intVec;
+    // // std::vector<int> input2;
+    // // std::vector<int> output;
+    // // int index = 0;
+    // // bool outputBool = false;
+
+}; // class MathTestsFixture
