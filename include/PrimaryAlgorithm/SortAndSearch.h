3c62579216ff6c68e7097e2c19d5b27e53ebcc13
luliu5 <luliu5@cisco.com>
2022-02-23 17:58:08 +0800

    2-23

--- include/PrimaryAlgorithm/SortAndSearch.h
+++ include/PrimaryAlgorithm/SortAndSearch.h
@@ -497,4 +497,61 @@ public:
 
         return {list.front(), list.back()};
     }
+
+    bool searchMatric(std::vector<std::vector<int>> nums, int target)
+    {
+        int col = nums.size();
+        if (col.size() == 0)
+        {
+            return false;
+        }
+        int row = nums[0].size();
+        if (row.size() == 0)
+        {
+            return false;
+        }
+
+        int i = 0;
+        int j = row -1;
+        int flag = nums[i][j];
+
+        while((i < col) && (j < row))
+        {
+            if (target == flag)
+            {
+                return true;
+            }
+            else if (target > flag)
+            {
+                flag = nums[i + 1][j];
+            }
+            else
+            {
+                flag = nums[i][j - 1];
+            }
+        }
+
+        return false;
+    }
+
+    int maxProfit(std::vector<int> prices)
+    {
+        int length = prices.size();
+        std::vector<std::vector<int>> dp(length, std::vector<int>(2, 0));
+        dp[0][0] = 0;
+        dp[0][1] = 0 - prices[0];
+        int i = 1;
+        int max = 0;
+        while(i < length)
+        {
+            dp[i][0] = dp[i - 1][0] > dp[i - 1][1] + prices[i] ? dp[i - 1][0] : dp[i - 1][1] + prices[i];
+            dp[i][1] = dp[i - 1][1] > dp[i - 1][0] - prices[i] ? dp[i - 1][1] : dp[i - 1][0] - prices[i];
+            if (dp[i][0] > max)
+            {
+                max = dp[i][0];
+            }
+        }
+
+        return max;
+    }
 };// class Solution
